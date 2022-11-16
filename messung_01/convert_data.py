d47751b97c2595c92823de7321f6a1424559dcee
Christopher Mahn <56981804+c-mahn@users.noreply.github.com>
2022-11-16 09:31:28 +0100

    Converted measurements to a standard-form

--- messung_01/convert_data.py
+++ messung_01/convert_data.py
@@ -7,25 +7,37 @@ import os
 
 # Functions
 
-def convert_file(filename, delimiter, columns, skiprows=[0]):
+def convert_file(filename, delimiter, skiprows=[0]):
     """
     This function converts a file into a .csv-file.
     """
+    sum_of_entries = 0
+    sum_of_rows = 0
     with open(os.path.join("data_raw", filename)) as file:  # Open the file
         data = file.readlines()  # Read the file
-    for line in data:  # Iterate over the lines
-        if(data.index(line) not in skiprows):  # Check if the line should be skipped
+    for i, line in enumerate(data):  # Iterate over the lines
+        if(i not in skiprows):  # Check if the line should be skipped
             line = line.split(delimiter)  # Split the line
-            for i, e in enumerate(line):  # Iterate over the elements
-                line[i] = e.strip()  # Remove the whitespaces
-            data[data.index(line)] = line  # Replace the line
-    with open(os.path.join("data", filename.split(".")[0] + ".csv"), "w") as file:  # Open the file
+            for j, e in enumerate(line):  # Iterate over the elements
+                line[j] = e.strip()  # Remove the whitespaces
+            data[i] = line  # Replace the line
+    with open(os.path.join("data_converted", filename.split(".")[0] + ".csv"), "w") as file:  # Open the file
         for line in data:  # Iterate over the lines
             if(data.index(line) not in skiprows):  # Check if the line should be skipped
-                for i, e in enumerate(line):  # Iterate over the elements
-                    if(i in columns):  # Check if the element should be written
-                        file.write(e + ";")  # Write the element
+                sum_of_rows += 1  # Increase the sum of rows
+                for j, entry in enumerate(line):  # Iterate over the elements
+                    sum_of_entries += 1  # Increase the sum of entries
+                    if(entry != ""):  # Check if the entry is empty
+                        if(j == 0):  # Check if the entry is the first entry
+                            file.write(entry)
+                        else:  # Check if the entry is not the first entry
+                            file.write(f';{entry}')  # Write the element
+                    else:
+                        pass
+                        # print(f'[WARN] Empty entry in line {data.index(line) + 1} in file "{filename}".')
                 file.write("\n")  # Write a new line
+    print(f'[INFO] The file {filename} has been converted.')
+    print(f'[INFO] The average number of entries per row is {sum_of_entries / sum_of_rows}.')
 
 
 # -----------------------------------------------------------------------------
@@ -37,3 +49,9 @@ def convert_file(filename, delimiter, columns, skiprows=[0]):
 # Beginning of main program
 
 if(__name__=='__main__'):
+    convert_file("Aramis_Belasten_Entlasten_1.ASC", "\t")
+    convert_file("Aramis_Belasten_Entlasten_2.csv", ",")
+    convert_file("Belastung_Entlastung_7000N.txt", "\t")
+    convert_file("Kabelkompensation_aus_Magnet_Heissluft.ASC", "\t")
+    convert_file("Netzkabel neben Messleitung.ASC", "\t")
+    convert_file("Schwingung.ASC", "\t")
